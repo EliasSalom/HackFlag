81775844f0c7ea3c60d2c7549847a5bde758885b
EliasSalom <elias.29@hotmail.com>
2020-09-17 12:14:29 +0530

    uploaded code

--- storage/framework/views/5d5d25c8ca8e95ff82b6dbaf7d196d1d3620ae90.php
+++
@@ -1,3 +0,0 @@
-<?php echo $__env->make('includes.header', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-<?php echo $__env->yieldContent('content'); ?>
-<?php echo $__env->make('includes.footer', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /home/demoarrj1n/public_html/Hackflag/Development/resources/views/layouts/front.blade.php ENDPATH**/ ?>
--- storage/framework/views/cfad58e51298065f2b289eed121460cb91c94d4c.php
+++
@@ -1,3 +0,0 @@
-<?php echo $__env->make('includes.adminheader', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-<?php echo $__env->yieldContent('content'); ?>
-<?php echo $__env->make('includes.adminfooter', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /home/demoarrj1n/public_html/Hackflag/Development/resources/views/layouts/admin.blade.php ENDPATH**/ ?>
